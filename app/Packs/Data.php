b9210c3013d4b09943daf6bced58cf1b6195b690
Eskelsen <dev@microframeworks.com>
2025-06-21 23:56:53 +0000

    Implements Data primary version

--- app/Packs/Data.php
+++ app/Packs/Data.php
@@ -2,15 +2,40 @@
 
 namespace App\Packs;
 
+use PDO;
+
 class Data
 {
+	public $conex;
+	
+	public function __construct()
+	{
+		try {
+			$options = [
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+				PDO::ATTR_PERSISTENT		 => true,
+				PDO::ATTR_TIMEOUT            => 60
+			];
+			$this->conex = new PDO("mysql:host=".HOST.";dbname=".DB, USER, PSW, $options);
+			$this->conex->exec('SET NAMES utf8mb4');
+			$this->conex->exec('SET time_zone = "-03:00";');
+		} catch (PDOException $e) {
+			error_log('Falha ao conectar com o banco de dados! Erro: ' . $e->getMessage());
+			$this->conex = false;
+			return false;
+		}
+		return $this->conex;
+	}
+	
 	public function tab($t)
 	{
 		$ok = query("SHOW TABLES LIKE '$t';");
 		if (!is_array($ok) OR empty($t)) {
 			return false;
 		}
-		$stmt = sqlExec("DESCRIBE $t");
+		$stmt = $this->sqlExec("DESCRIBE $t");
 		$data = $stmt ? $stmt->fetchAll() : false;
 		return is_array($data) ? array_column($data,'Field') : false;
 	}
@@ -32,26 +57,21 @@ class Data
 
 	public function quote($in)
 	{
-		global $conex;
-		return ($conex) ? $conex->quote($in) : false;
+		return ($this->conex) ? $this->conex->quote($in) : false;
 	}
 
 	public function insert($t,$vs)
 	{
-		global $conex;
-		if (!$conex) {
-			return false;
-		}
 		$f = implode(',',array_keys($vs));
 		$v = array_values($vs);
 		$h      = implode(',',array_fill(0,count($v),'?'));
-		$stmt   = sqlExec("INSERT INTO $t ($f) VALUES ($h);", $v);
-		return $stmt ? $conex->lastInsertId() : false;
+		$stmt   = $this->sqlExec("INSERT INTO $t ($f) VALUES ($h);", $v);
+		return $this->conex && $stmt ? $this->conex->lastInsertId() : false;
 	}
 
 	public function field($t,$f,$cond = null,$v = false)
 	{
-		$stmt = sqlExec("SELECT $f FROM $t $cond;",$v);
+		$stmt = $this->sqlExec("SELECT $f FROM $t $cond;",$v);
 		return $stmt ? $stmt->fetchColumn() : false;
 	}
 
@@ -68,20 +88,20 @@ class Data
 
 	public function selectRow($t,$f = '*',$cond = null,$v = false)
 	{
-		$stmt = sqlExec("SELECT $f FROM $t $cond;",$v);
+		$stmt = $this->sqlExec("SELECT $f FROM $t $cond;",$v);
 		return $stmt ? $stmt->fetch() : false;
 	}
 
 	public function selectColumn($t,$f,$cond = null,$v = false)
 	{
-		$stmt = sqlExec("SELECT $f FROM $t $cond;",$v);
+		$stmt = $this->sqlExec("SELECT $f FROM $t $cond;",$v);
 		$data = $stmt ? $stmt->fetchAll() : false;
 		return is_array($data) ? array_column($data,$f) : false;
 	}
 
 	public function selectAll($t,$f = '*',$cond = null,$v = false)
 	{
-		$stmt = sqlExec("SELECT $f FROM $t $cond;",$v);
+		$stmt = $this->sqlExec("SELECT $f FROM $t $cond;",$v);
 		return $stmt ? $stmt->fetchAll() : false;
 	}
 
@@ -98,7 +118,7 @@ class Data
 	public function selectThing($t,$f,$op,$cond = null,$v = false)
 	{
 		$field = "$op($f)";
-		$stmt = sqlExec("SELECT $field FROM $t $cond;",$v);
+		$stmt = $this->sqlExec("SELECT $field FROM $t $cond;",$v);
 		$n = $stmt ? $stmt->fetch() : false;
 		return (isset($n[$field])) ? $n[$field] : 0;
 	}
@@ -107,7 +127,7 @@ class Data
 	{
 		[$f,$fvs] = parameterfy($a);
 		$vs = array_merge($fvs,$cvs);
-		$stmt = sqlExec("UPDATE $t SET $f WHERE $c;",$vs);
+		$stmt = $this->sqlExec("UPDATE $t SET $f WHERE $c;",$vs);
 		return $stmt ? $stmt->rowCount() : false;
 	}
 
@@ -122,37 +142,37 @@ class Data
 
 	public function increment($t,$f,$c,$v = [])
 	{
-		$stmt = sqlExec("UPDATE $t SET $f=$f+1 WHERE $c;",$v);
+		$stmt = $this->sqlExec("UPDATE $t SET $f=$f+1 WHERE $c;",$v);
 		return $stmt ? $stmt->rowCount() : false;
 	}
 
 	public function decrement($t,$f,$c,$v = [])
 	{
-		$stmt = sqlExec("UPDATE $t SET $f=$f-1 WHERE $c;",$v);
+		$stmt = $this->sqlExec("UPDATE $t SET $f=$f-1 WHERE $c;",$v);
 		return $stmt ? $stmt->rowCount() : false;
 	}
 
 	public function activate($t,$f,$c,$v = [])
 	{
-		$stmt = sqlExec("UPDATE $t SET $f=1 WHERE $c;",$v);
+		$stmt = $this->sqlExec("UPDATE $t SET $f=1 WHERE $c;",$v);
 		return $stmt ? $stmt->rowCount() : false;
 	}
 
 	public function deactivate($t,$f,$c,$v = [])
 	{
-		$stmt = sqlExec("UPDATE $t SET $f=0 WHERE $c;",$v);
+		$stmt = $this->sqlExec("UPDATE $t SET $f=0 WHERE $c;",$v);
 		return $stmt ? $stmt->rowCount() : false;
 	}
 
 	public function del($t,$c,$v = [])
 	{
-		$stmt = sqlExec("DELETE FROM $t WHERE $c;",$v);
+		$stmt = $this->sqlExec("DELETE FROM $t WHERE $c;",$v);
 		return $stmt ? $stmt->rowCount() : false;
 	}
 
 	public function query($q)
 	{
-		if ($stmt = sqlExec($q)) {
+		if ($stmt = $this->sqlExec($q)) {
 			if ($affected = $stmt->rowCount()) {
 				return ($affected>1) ? $stmt->fetchAll() : $stmt->fetch();
 			}
@@ -163,52 +183,27 @@ class Data
 
 	public function sqlExec($sql, $v = false, $recursive = false)
 	{
-		global $conex;
-		if (!$conex) {
-			microlog('[MicroDB] Sem conexão.', TMP . 'sql.txt');
+		if (!$this->conex) {
 			return false;
 		}
-		DEV ? microlog('[MicroDB] SQL: ' . $sql, TMP . 'sql.txt') : null;
-		if ($v AND DEV) {
-			microlog('[MicroDB] Args: ' . json_encode($v), TMP . 'sql.txt');
-		}
 		try {
-			$stmt = $conex->prepare($sql);
+			$stmt = $this->conex->prepare($sql);
 			$made = $v ? $stmt->execute($v) : $stmt->execute();
 		} catch (PDOException $e) {
 			if (($e->getCode()=='HY000') AND ($recursive==false)) {
-				$conex = reconnect();
-				return sqlExec($sql, $v, true);
+				$this->conex = reconnect();
+				return $this->sqlExec($sql, $v, true);
 			}
-			microlog('[MicroDB] Falha ao realizar operação com o banco de dados! Erro: ' . $e->getMessage());
+			error_log('[MicroDB] Falha ao realizar operação com o banco de dados! Erro: ' . $e->getMessage());
 		}
 		return empty($made) ? false : $stmt;
 	}
 
 	public function reconnect()
 	{
-		global $conex, $start_time;
-		$acc_id = getenv('ACC_ID');
-		$acc_in_use = ($acc_id) ? ' acc_id [' . $acc_id . ']' : '';
+		global $start_time;
 		$execution_time = round(microtime(true) - $start_time,2);
-		microlog("[MicroDB] reconnect $execution_time" . $acc_in_use, TMP . 'sql.txt');
-		try {
-
-			$options = [
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_EMULATE_PREPARES   => false,
-				PDO::ATTR_PERSISTENT		 => true, # Watching
-				PDO::ATTR_TIMEOUT            => 60
-			];
-			
-			$conex = new PDO("mysql:host=".HOST.";dbname=".DB, USER, PSW, $options);
-			$conex->exec('SET NAMES utf8');
-			$conex->exec('SET time_zone = "-03:00";');
-			return $conex;
-		} catch (PDOException $e) {  
-			microlog('[MicroDB] Falha ao reconectar com o banco de dados! Erro: ' . $e->getMessage());
-			return false;
-		}
+		error_log("[MicroDB] reconnect $execution_time", TMP . 'sql.txt');
+		return new Data();
 	}
 }
